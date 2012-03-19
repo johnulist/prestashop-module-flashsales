3bced162f0a4aaa2f08156dcc7b877bbdc325162
Pierrick CAEN <prcaen@gmail.com>
2012-03-19 09:15:45 +0100

    Adding mail alert on a offer.

--- frontend/controllers/FlashSalesOfferOldController.php
+++ frontend/controllers/FlashSalesOfferOldController.php
@@ -9,6 +9,49 @@ class FlashSalesOfferOldControllerCore extends FrontController
 		$this->canonicalRedirection();
 
 		parent::preProcess();
+		
+		if (Tools::isSubmit('SubmitMailAlert'))
+		{
+			$id_flashsales_offer = (int)(Tools::getValue('id_flashsales_offer'));
+			$insert = false;
+
+			if(!empty($id_flashsales_offer))
+			{
+				if (!self::$cookie->isLogged())
+				{
+					$customer_email = Tools::getValue('customer_email');
+					
+					if (empty($customer_email) || !Validate::isEmail($customer_email) || $customer_email == 'your@email.com')
+					{
+						// Empty email
+						$this->errors[] = Tools::displayError('Your e-mail address is invalid');
+					}
+					else
+					{
+						$id_customer = (int)Db::getInstance()->getValue('SELECT id_customer FROM '._DB_PREFIX_.'customer WHERE email=\''.pSQL($customer_email).'\' AND is_guest=0');
+						if (!Db::getInstance()->ExecuteS('SELECT * FROM `'._DB_PREFIX_.'flashsales_offer_mailalert` WHERE `id_customer` = '.(int)($id_customer).' AND `customer_email` = \''.pSQL($customer_email).'\' AND `id_flashsales_offer` = '.(int)($id_flashsales_offer)))
+							$insert = true;
+						else
+						{
+							// Already in DB.
+							$this->errors[] = Tools::displayError('You\'re already register for this offer.');
+						}
+					}
+				}
+				else
+				{
+					$id_customer = (int)($cookie->id_customer);
+					$customer_email = Db::getInstance()->getValue('SELECT email FROM '._DB_PREFIX_.'customer WHERE id_customer=\''.(int)($id_customer).'\' AND is_guest=0');
+					$insert = true;
+				}
+
+				if($insert)
+				{
+					Db::getInstance()->Execute('REPLACE INTO `'._DB_PREFIX_.'flashsales_offer_mailalert` (`id_flashsales_offer`, `id_customer`, `customer_email`) VALUES ('.(int)($id_flashsales_offer).', '.(int)($id_customer).', \''.pSQL($customer_email).'\'');
+					self::$smarty->assign('mailalert_confirm', true);
+				}
+			}
+		}
 	}
 
 	public function process()
